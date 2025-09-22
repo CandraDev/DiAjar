40e73f75bccf133c4b16f04e022bfcab930f39aa
candradev <candrasetiadev@gmail.com>
2025-09-22 14:19:32 +0700

    feat(user): add avatar google integration

--- app/Http/Controllers/GoogleAuthController.php
+++ app/Http/Controllers/GoogleAuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Laravel\Socialite\Facades\Socialite;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class GoogleAuthController extends Controller
 {
@@ -13,29 +15,30 @@ class GoogleAuthController extends Controller
         return Socialite::driver('google')->redirect();
     }
 
-public function callback()
-{
-    $googleUser = Socialite::driver('google')->stateless()->user();
+    public function callback()
+    {
+        $googleUser = Socialite::driver('google')->stateless()->user();
 
-    $user = User::updateOrCreate(
-        ['email' => $googleUser->getEmail()],
-        [
-            'name' => $googleUser->getName(),
-            'google_id' => $googleUser->getId(),
-            'password' => bcrypt(str()->random(16)),
-        ]
-    );
+        // Ambil URL avatar Google
+        $avatarUrl = $googleUser->getAvatar();
 
-    Auth::login($user);
+        // Simpan avatar ke storage/public/avatars
+        $avatarName = Str::random(20) . '.jpg'; // nama random
+        $avatarContents = file_get_contents($avatarUrl); // ambil kontennya
+        Storage::disk('public')->put('avatars/' . $avatarName, $avatarContents);
 
-    return redirect(route('dashboard'));
+        $user = User::updateOrCreate(
+            ['email' => $googleUser->getEmail()],
+            [
+                'name' => $googleUser->getName(),
+                'google_id' => $googleUser->getId(),
+                'password' => bcrypt(Str::random(16)),
+                'avatar' => 'avatars/' . $avatarName, // simpan path di DB
+            ]
+        );
 
-    // return dd([
-    //     'googleUser'   => $googleUser,
-    //     'db_user'      => $user,
-    //     'auth_user'    => Auth::user(),
-    //     'session_id'   => session()->getId(),
-    // ]);
-}
+        Auth::login($user);
 
+        return redirect(route('dashboard'));
+    }
 }
